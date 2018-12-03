0a3f0f7e0e1dad62c5f240014e2e7ceb277850f6
bradleykenny <bkenny283@gmail.com>
2018-12-04 08:07:28 +1100

    Have checkboxes working as intended... finally.

--- plannerProject/InfoController.swift
+++ plannerProject/InfoController.swift
@@ -42,15 +42,16 @@ class InfoController: UIViewController {
 				if (task.check) {
 					checkBox = UIImage(named: "checkFilled@60")
 				}
-				let checkBtn = UIButton()
+				let checkBtn = CheckUIButton(taskFor: task, checkImage: checkBox!)
 				checkBtn.frame = CGRect.init(x: 20, y: yPos, width: 30, height: 30)
 				checkBtn.setImage(checkBox, for: .normal)
 				if (task.check) {
 					checkBtn.imageView?.image = checkBtn.imageView?.image?.withRenderingMode(.alwaysTemplate)
 					checkBtn.imageView?.tintColor = UIColor(red: 105/255, green: 197/255, blue: 60/255, alpha: 1.0)
 				}
+				checkBtn.addTarget(self, action: #selector(self.changeCheck(_:)), for: .touchUpInside)
 				self.view.addSubview(checkBtn)
-				
+	
 				// task label
 				let taskField = UITextField()
 				taskField.returnKeyType = .done
@@ -60,12 +61,19 @@ class InfoController: UIViewController {
 				taskField.frame = CGRect(x: 60, y: yPos-5, width:250, height: 40)
 				yPos += 35
 				self.view.addSubview(taskField)
-				
-				// TODO: implement some way of recognising taps and
-				//       changing the images to reflect state of task
 			}
  		}
     }
+	
+	@objc func changeCheck(_ sender: CheckUIButton!) {
+		sender.taskFor.check = !sender.taskFor.check
+		if (sender.taskFor.check) {
+			sender.checkImage = UIImage(named: "checkFilled@60")!
+		} else {
+			sender.checkImage = UIImage(named: "check@60")!
+		}
+		sender.setImage(sender.checkImage, for: .normal)
+	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		// this won't work?
@@ -78,5 +86,21 @@ class InfoController: UIViewController {
 		print("Should be closing")
 		self.view.endEditing(true)
 		return false
+	}
+}
+
+// use this subclass, otherwise cannot access what task the checkBtn is for
+class CheckUIButton: UIButton {
+	var taskFor: Task
+	var checkImage: UIImage
+	
+	init(taskFor: Task, checkImage: UIImage) {
+		self.taskFor = taskFor
+		self.checkImage = checkImage
+		super.init(frame: .zero)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
 	}
 }
